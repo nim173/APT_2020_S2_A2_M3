b3ba0ef35ed3ff088469e218478eae9414657033
[Ryan Peterson s3817175] <S3817175@student.rmit.edu.au>
2020-09-09 22:07:51 +1000

    Menu method

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -4,29 +4,34 @@ using std::cout;
 using std::endl;
 
 void show_credits();
+void show_menu();
 
 int main(void) {
-    bool loop = true;
-    do {
-        cout << "Welcome to Azul" << endl;
-        cout << "---------------" << endl << endl;
-        cout << "Menu" << endl;
-        cout << "----" << endl;
-        cout << "1. New Game" << endl
-            << "2. Load Game" << endl
-            << "3. Credits (Show student information)"
-            << "4. Quit" << endl;
-        int input;
-        std::cin >> input;
-        if (input == 3){
-            show_credits();
-        } else if (input == 4) {
-            loop = false;
-        }
+   show_menu(); 
+   bool loop = true;
+   do {
+      int input;
+      std::cin >> input;
+      if (input == 3) {
+         show_credits();
+      } else if (input == 4) {
+         loop = false;
+      }
 
-    } while (loop);
+   } while (loop);
 }
 
-void show_credits(){
-    
+void show_credits() {
+}
+
+void show_menu() {
+   cout << "Welcome to Azul" << endl;
+   cout << "---------------" << endl
+        << endl;
+   cout << "Menu" << endl;
+   cout << "----" << endl;
+   cout << "1. New Game" << endl
+        << "2. Load Game" << endl
+        << "3. Credits (Show student information)"
+        << "4. Quit" << endl;
 }
