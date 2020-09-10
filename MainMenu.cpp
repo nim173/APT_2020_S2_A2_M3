6be2950521e2a755a423fb1cc7c0b72b5d1bc9ed
seanaaronmorais <seanaaronmorais@gmail.com>
2020-09-10 13:49:16 +0800

    main menu with cred

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -1,30 +1,52 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
 
 void show_credits();
 void show_menu();
 
-int main(void) {
-   show_menu(); 
+int main(void)
+{
+
    bool loop = true;
-   do {
-      int input;
+   do
+   {
+      int input=0;
+      show_menu();
+      std::cout << "> ";
       std::cin >> input;
-      if (input == 3) {
+      if (input == 3)
+      {
          show_credits();
-      } else if (input == 4) {
+      }
+      else if (input == 4)
+      {
          loop = false;
       }
 
    } while (loop);
 }
 
-void show_credits() {
+void show_credits()
+{
+   string line;
+   std::ifstream myFile("credits.txt");
+   if (myFile.is_open())
+   {
+      while (getline(myFile, line))
+      {
+         cout << line << endl;
+      }
+   }
+   myFile.close();
 }
 
-void show_menu() {
+void show_menu()
+{
    cout << "Welcome to Azul" << endl;
    cout << "---------------" << endl
         << endl;
