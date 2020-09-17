eae4146245a768a7aa1ce0881335b8ade72f3d83
sheanaaron <s3808041@student.rmit.edu.au>
2020-09-17 16:36:03 +0800

    defined tile colours

--- Types.h
+++ Types.h
@@ -5,4 +5,14 @@
 #define NO_OF_PLAYERS 2
 #define NO_OF_ROUNDS 5
 
+#define R "Red"
+#define Y "Yellow"
+#define B "Dark Blue"
+#define L "Light Blue"
+#define U "Black"
+#define F "first-player"
+//If we dont need this we'll take it out - S
+#define E "."
+
+
 #endif // TYPES
