82bd04c655197200f269d1c0acabb82793b4a5a6
sheanaaron <s3808041@student.rmit.edu.au>
2020-09-18 02:41:30 +0800

    Updated enum

--- Types.h
+++ Types.h
@@ -4,15 +4,8 @@
 #define MOSAIC_DIM 5
 #define NO_OF_PLAYERS 2
 #define NO_OF_ROUNDS 5
-
-#define R "Red"
-#define Y "Yellow"
-#define B "Dark Blue"
-#define L "Light Blue"
-#define U "Black"
-#define F "first-player"
-//If we dont need this we'll take it out - S
-#define E "."
+//E represents an empty slot
+enum Tile {R, Y, B, L, U, F, E}
 
 
 #endif // TYPES
