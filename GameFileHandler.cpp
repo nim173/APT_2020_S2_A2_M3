ea43533ebdbce663222bd56aaddd26451b7df9d1
sheanaaron <s3808041@student.rmit.edu.au>
2020-09-24 01:35:06 +0800

    added initial file creation method

--- GameFileHandler.cpp
+++ GameFileHandler.cpp
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-#include "GameFileHandler.h"
-
-GameFileHandler::GameFileHandler()
-{    
-}
-
-GameFileHandler::~GameFileHandler()
-{
-
-}
-
-void GameFileHandler::save()
-{
-
-}
-
-void GameFileHandler::load()
-{
-
-}
-
-=======
 #include <vector>
 #include <fstream>
 #include <string>
@@ -28,66 +5,100 @@ void GameFileHandler::load()
 #include "Types.h"
 #include "GameFileHandler.h"
 
+using std::endl;
 using std::string;
 
 GameFileHandler::GameFileHandler()
 {
-    
 }
 
 GameFileHandler::~GameFileHandler()
 {
-
 }
 
-void GameFileHandler::save()
+// void GameFileHandler::saveGame()
+// {
+//     //call respective methods
+// }
+
+void GameFileHandler::createFile(string fileName, Player *players[NO_OF_PLAYERS], vector<string> *turns)
 {
+    std::ofstream file;
+    file.open(fileName);
+    file << DEFAULT_TILEBAG_FILE << endl;
+    int arrSize = sizeof(players) / sizeof(players[0]);
+    for (int i = 0; i < arrSize; ++i)
+    {
+        file << players[i]->getName();
+    }
+
+    for (int i=0; i != turns->size(); i++ )
+    {
+        file <<  turns->at(i)  << ' ' << endl;
+    }
 
 }
 
-void GameFileHandler::load()
+void GameFileHandler::saveTurn(string file, string inputFactory, string inputTile, string inputStorageRow)
 {
+    std::ifstream inFile;
+    inFile.open(file);
 
 }
 
-bool GameFileHandler::loadTileBag(string file, LinkedList* tilebag) {
+void GameFileHandler::loadGame()
+{
+}
+
+bool GameFileHandler::loadTileBag(string file, LinkedList *tilebag)
+{
     bool result = false;
     std::ifstream inFile;
     inFile.open(file);
-    if (inFile.good()) {
+    if (inFile.good())
+    {
         string tiles;
         std::getline(inFile, tiles);
-        for (unsigned int i = 0; i < tiles.length(); ++i) {
+        for (unsigned int i = 0; i < tiles.length(); ++i)
+        {
             tilebag->addBack(tiles.at(i));
         }
         result = true;
-    } else {
+    }
+    else
+    {
         // file not found
     }
     inFile.close();
     return result;
 }
 
-bool GameFileHandler::loadMosaic(string file, Mosaic mosaic) {
+bool GameFileHandler::loadMosaic(string file, Mosaic mosaic)
+{
     bool result = false;
     std::ifstream inFile;
     inFile.open(file);
-    if (inFile.good()) {
+    if (inFile.good())
+    {
         char ch;
-        for (int i = 0; i < MOSAIC_DIM; ++i) {
-            for (int j = 0; j < MOSAIC_DIM;) {
+        for (int i = 0; i < MOSAIC_DIM; ++i)
+        {
+            for (int j = 0; j < MOSAIC_DIM;)
+            {
                 inFile.get(ch);
                 // accounting for newline characters
-                if (ch != '\n' && ch!= '\r' && ch != ' ') {
+                if (ch != '\n' && ch != '\r' && ch != ' ')
+                {
                     mosaic[i][j] = ch;
                     ++j;
                 }
             }
         }
-    } else {
+    }
+    else
+    {
         // file not found
     }
     inFile.close();
     return result;
 }
->>>>>>> aedcc130528e470d646fb82c12e5870018348ad0
--- GameFileHandler.h
+++ GameFileHandler.h
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 #ifndef GAME_FILE_HANDLER
 #define GAME_FILE_HANDLER
 
 #include <string>
 #include "Types.h"
+#include "DELinkedList.h"
+
 
 using std::string;
 
@@ -13,44 +14,20 @@ public:
     GameFileHandler();
     ~GameFileHandler();
 
-    void save(string fileName, string inputFactory, string inputTile, string inputStorageRow );
-    void load();
+    void saveGame();
+void GameFileHandler::createFile(string fileName, Player *players[NO_OF_PLAYERS], vector<string> *turns)
+    void saveTurn(string fileName, string inputFactory, string inputTile, string inputStorageRow);
+
+    void loadGame(string fileName);
+
+    bool loadTileBag(DEFAULT_TILEBAG_FILE, LinkedList tilebag);
+
+    void loadMosaic(DEFAULT_MOSAIC_FILE, Tile* defaultMosaicGrid);
+
+
 
 private:
-   
+
 };
 
-=======
-#ifndef GAME_FILE_HANDLER
-#define GAME_FILE_HANDLER
-
-#include <string>
-#include "Player.h"
-#include "Types.h"
-#include "DELinkedList.h"
-
-using std::string;
-using std::vector;
-
-class GameFileHandler {
-public:
-    GameFileHandler();
-    ~GameFileHandler();
-
-    bool loadTileBag(string file, LinkedList* tilebag);
-    bool loadMosaic(string file, Mosaic mosaic);
-
-    void save();
-    void load();
-
-private:
-
-
-
-private:
-    std::ifstream inFile;
-};
-
-
->>>>>>> aedcc130528e470d646fb82c12e5870018348ad0
 #endif // GAME_FILE_HANDLER
